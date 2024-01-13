62fcb0e9f7aaa6e1e14bff169a6d4ce303f9dbda
01Derek10 <131251445+01Derek10@users.noreply.github.com>
2024-01-13 12:42:40 +0100

    odontograma

---
+++ dentiapp/src/Vista/OdontogramaDialog.java
@@ -0,0 +1,51 @@
+package Vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class OdontogramaDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			OdontogramaDialog dialog = new OdontogramaDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setUndecorated(true);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public OdontogramaDialog() {
+		setLocationRelativeTo(null);	
+		setResizable(false);
+		setUndecorated(true);
+		setBounds(100, 100, 953, 600);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setIcon(new ImageIcon(adminFrame.class.getResource("/fotos/odontograma.PNG")));
+		lblFondo.setBounds(0, 0, 954, 594);
+		contentPanel.add(lblFondo);
+	}
+}
