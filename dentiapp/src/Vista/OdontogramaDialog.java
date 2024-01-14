4ef55755036843dc79c44ddc4fb6fa5c91078aab
Francisco Ruiz Nieto <97099217+franruizn@users.noreply.github.com>
2024-01-14 18:09:05 +0100

    Botones mandados al fondo

--- dentiapp/src/Vista/OdontogramaDialog.java
+++ dentiapp/src/Vista/OdontogramaDialog.java
@@ -53,136 +53,9 @@ public class OdontogramaDialog extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		JButton btnd2 = new JButton("");
-		btnd2.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd2.setBounds(303, 207, 18, 21);
-		contentPanel.add(btnd2);
-		
-		JButton btnd3 = new JButton("3");
-		btnd3.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd3.setBounds(356, 225, 41, 21);
-		contentPanel.add(btnd3);
-		
-		JButton btnd4 = new JButton("4");
-		btnd4.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd4.setBounds(377, 254, 41, 21);
-		contentPanel.add(btnd4);
-		
-		JButton btnd5 = new JButton("5");
-		btnd5.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd5.setBounds(377, 285, 41, 21);
-		contentPanel.add(btnd5);
-		
-		JButton btnd6 = new JButton("6");
-		btnd6.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd6.setBounds(224, 156, 41, 21);
-		contentPanel.add(btnd6);
-		
-		JButton btnd7 = new JButton("7");
-		btnd7.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd7.setBounds(185, 194, 41, 21);
-		contentPanel.add(btnd7);
-		
-		JButton btnd8 = new JButton("8");
-		btnd8.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd8.setBounds(168, 225, 41, 21);
-		contentPanel.add(btnd8);
-		
-		JButton btnd9 = new JButton("9");
-		btnd9.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd9.setBounds(157, 254, 41, 21);
-		contentPanel.add(btnd9);
-		
-		JButton btnd10 = new JButton("10");
-		btnd10.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd10.setBounds(141, 285, 41, 21);
-		contentPanel.add(btnd10);
-		
-		JButton btnd11 = new JButton("11");
-		btnd11.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd11.setBounds(377, 326, 41, 21);
-		contentPanel.add(btnd11);
-		
-		JButton btnd12 = new JButton("12");
-		btnd12.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd12.setBounds(366, 363, 41, 21);
-		contentPanel.add(btnd12);
-		
-		JButton btnd13 = new JButton("13");
-		btnd13.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd13.setBounds(356, 391, 41, 21);
-		contentPanel.add(btnd13);
-		
-		JButton btnd14 = new JButton("14");
-		btnd14.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd14.setBounds(330, 422, 41, 21);
-		contentPanel.add(btnd14);
-		
-		JButton btnd15 = new JButton("15");
-		btnd15.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd15.setBounds(314, 449, 41, 21);
-		contentPanel.add(btnd15);
-		
-		JButton btnd16 = new JButton("16");
-		btnd16.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd16.setBounds(157, 316, 41, 21);
-		contentPanel.add(btnd16);
-		
-		JButton btnd17 = new JButton("17");
-		btnd17.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd17.setBounds(157, 349, 41, 21);
-		contentPanel.add(btnd17);
-		
-		JButton btnd18 = new JButton("18");
-		btnd18.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd18.setBounds(168, 380, 41, 21);
-		contentPanel.add(btnd18);
-		
-		JButton btnd19 = new JButton("19");
-		btnd19.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd19.setBounds(203, 411, 41, 21);
-		contentPanel.add(btnd19);
-		
-		JButton btnd20 = new JButton("20");
-		btnd20.setFont(new Font("Tahoma", Font.PLAIN, 8));
-		btnd20.setBounds(224, 449, 41, 21);
-		contentPanel.add(btnd20);	
-		
-
-		JLabel lblDatos = new JLabel("DATOS");
-		lblDatos.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDatos.setFont(new Font("SansSerif", Font.PLAIN, 25));
-		lblDatos.setBounds(629, 132, 146, 33);
-		contentPanel.add(lblDatos);
-		
-		JLabel lblNumDiente = new JLabel("nº Diente :");
-		lblNumDiente.setBounds(538, 228, 52, 13);
-		contentPanel.add(lblNumDiente);
-		
-		JLabel lblDescripcion = new JLabel("Descripción :");
-		lblDescripcion.setBounds(674, 228, 60, 13);
-		contentPanel.add(lblDescripcion);
-		
-		txtNumDiente = new JTextField();
-		txtNumDiente.setBounds(593, 225, 34, 19);
-		contentPanel.add(txtNumDiente);
-		txtNumDiente.setColumns(10);
-		
-		JTextArea txtaDescripcion = new JTextArea();
-		txtaDescripcion.setBounds(744, 222, 119, 69);
-		contentPanel.add(txtaDescripcion);
-		
 		JCheckBox chckbxAusencias = new JCheckBox("Ausencias");
 		chckbxAusencias.setBounds(538, 315, 86, 21);
 		contentPanel.add(chckbxAusencias);
-		
-		JCheckBox chckbxCaries = new JCheckBox("Caries");
-		chckbxCaries.setBounds(652, 314, 93, 22);
-		contentPanel.add(chckbxCaries);
-		
-		JCheckBox chckbxImplantes = new JCheckBox("Implantes");
-		chckbxImplantes.setBounds(770, 315, 93, 22);
-		contentPanel.add(chckbxImplantes);
 		
 		JCheckBox chckbxProtesis = new JCheckBox("Prótesis");
 		chckbxProtesis.setBounds(538, 361, 86, 22);
@@ -192,22 +65,54 @@ public class OdontogramaDialog extends JDialog {
 		chckbxSangrado.setBounds(652, 362, 93, 21);
 		contentPanel.add(chckbxSangrado);
 		
-		JCheckBox chckbxRayos = new JCheckBox("Rayos X");
-		chckbxRayos.setBounds(770, 362, 93, 21);
-		contentPanel.add(chckbxRayos);
+		JTextArea txtaDescripcion = new JTextArea();
+		txtaDescripcion.setBounds(744, 222, 119, 69);
+		contentPanel.add(txtaDescripcion);
+		
+		JButton btnSalir = new JButton("SALIR");
+		btnSalir.setBounds(847, 27, 85, 21);
+		contentPanel.add(btnSalir);
+		
+		JCheckBox chckbxCaries = new JCheckBox("Caries");
+		chckbxCaries.setBounds(652, 314, 93, 22);
+		contentPanel.add(chckbxCaries);
+		
+		JCheckBox chckbxImplantes = new JCheckBox("Implantes");
+		chckbxImplantes.setBounds(770, 315, 93, 22);
+		contentPanel.add(chckbxImplantes);
 		
 		JButton btnGuardar = new JButton("GUARDAR");
 		btnGuardar.setBounds(652, 435, 93, 21);
 		contentPanel.add(btnGuardar);
+		
+		JLabel lblDescripcion = new JLabel("Descripción :");
+		lblDescripcion.setBounds(674, 228, 60, 13);
+		contentPanel.add(lblDescripcion);
+		
+		JCheckBox chckbxRayos = new JCheckBox("Rayos X");
+		chckbxRayos.setBounds(770, 362, 93, 21);
+		contentPanel.add(chckbxRayos);
 		
 		JLabel lblPaciente = new JLabel("Paciente");
 		lblPaciente.setFont(new Font("SansSerif", Font.PLAIN, 20));
 		lblPaciente.setBounds(593, 175, 223, 37);
 		contentPanel.add(lblPaciente);
 		
-		JButton btnSalir = new JButton("SALIR");
-		btnSalir.setBounds(847, 27, 85, 21);
-		contentPanel.add(btnSalir);
+		txtNumDiente = new JTextField();
+		txtNumDiente.setBounds(593, 225, 34, 19);
+		contentPanel.add(txtNumDiente);
+		txtNumDiente.setColumns(10);
+		
+		JLabel lblNumDiente = new JLabel("nº Diente :");
+		lblNumDiente.setBounds(538, 228, 52, 13);
+		contentPanel.add(lblNumDiente);
+		
+
+		JLabel lblDatos = new JLabel("DATOS");
+		lblDatos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDatos.setFont(new Font("SansSerif", Font.PLAIN, 25));
+		lblDatos.setBounds(629, 132, 146, 33);
+		contentPanel.add(lblDatos);
 		
 		
 		
@@ -215,6 +120,101 @@ public class OdontogramaDialog extends JDialog {
 		lblFondo.setIcon(new ImageIcon(adminFrame.class.getResource("/fotos/odontograma.PNG")));
 		lblFondo.setBounds(0, 0, 954, 594);
 		contentPanel.add(lblFondo);
+		
+		JButton btnd2 = new JButton("");
+		btnd2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd2.setBounds(303, 207, 18, 21);
+		contentPanel.add(btnd2);
+		
+		JButton btnd3 = new JButton("3");
+		btnd3.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd3.setBounds(313, 225, 18, 21);
+		contentPanel.add(btnd3);
+		
+		JButton btnd4 = new JButton("4");
+		btnd4.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd4.setBounds(330, 250, 18, 21);
+		contentPanel.add(btnd4);
+		
+		JButton btnd5 = new JButton("5");
+		btnd5.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd5.setBounds(336, 278, 18, 21);
+		contentPanel.add(btnd5);
+		
+		JButton btnd6 = new JButton("6");
+		btnd6.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd6.setBounds(269, 194, 14, 21);
+		contentPanel.add(btnd6);
+		
+		JButton btnd7 = new JButton("7");
+		btnd7.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd7.setBounds(249, 206, 16, 21);
+		contentPanel.add(btnd7);
+		
+		JButton btnd8 = new JButton("8");
+		btnd8.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd8.setBounds(236, 225, 15, 21);
+		contentPanel.add(btnd8);
+		
+		JButton btnd9 = new JButton("9");
+		btnd9.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd9.setBounds(222, 251, 23, 21);
+		contentPanel.add(btnd9);
+		
+		JButton btnd10 = new JButton("10");
+		btnd10.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd10.setBounds(218, 280, 20, 21);
+		contentPanel.add(btnd10);
+		
+		JButton btnd11 = new JButton("11");
+		btnd11.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd11.setBounds(329, 324, 18, 21);
+		contentPanel.add(btnd11);
+		
+		JButton btnd12 = new JButton("12");
+		btnd12.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd12.setBounds(328, 349, 20, 21);
+		contentPanel.add(btnd12);
+		
+		JButton btnd13 = new JButton("13");
+		btnd13.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd13.setBounds(314, 370, 20, 21);
+		contentPanel.add(btnd13);
+		
+		JButton btnd14 = new JButton("14");
+		btnd14.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd14.setBounds(299, 384, 18, 21);
+		contentPanel.add(btnd14);
+		
+		JButton btnd15 = new JButton("15");
+		btnd15.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd15.setBounds(286, 387, 11, 21);
+		contentPanel.add(btnd15);
+		
+		JButton btnd16 = new JButton("16");
+		btnd16.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd16.setBounds(222, 324, 15, 21);
+		contentPanel.add(btnd16);
+		
+		JButton btnd17 = new JButton("17");
+		btnd17.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd17.setBounds(224, 351, 14, 21);
+		contentPanel.add(btnd17);
+		
+		JButton btnd18 = new JButton("18");
+		btnd18.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd18.setBounds(240, 370, 11, 21);
+		contentPanel.add(btnd18);
+		
+		JButton btnd19 = new JButton("19");
+		btnd19.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd19.setBounds(257, 383, 11, 21);
+		contentPanel.add(btnd19);
+		
+		JButton btnd20 = new JButton("20");
+		btnd20.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnd20.setBounds(273, 387, 10, 21);
+		contentPanel.add(btnd20);	
 
 		// Establecemos el cursor de mano
 		btnd2.setOpaque(false);
