02dd515dcb3f27fae92b712b604c243b98027745
01Derek10 <131251445+01Derek10@users.noreply.github.com>
2024-02-06 11:07:08 +0100

    buscar paciente con cmbx

--- dentiapp/src/Vista/BuscarPacienteDialog.java
+++ dentiapp/src/Vista/BuscarPacienteDialog.java
@@ -2,18 +2,30 @@ package Vista;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Controlador.ControladorSQL;
 
 public class BuscarPacienteDialog extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private ControladorSQL con = new ControladorSQL();
+	private ArrayList<String[]> consultas = new ArrayList<>();
+
 
 	/**
 	 * Launch the application.
@@ -28,6 +40,7 @@ public class BuscarPacienteDialog extends JDialog {
 		}
 	}
 
+
 	/**
 	 * Create the dialog.
 	 */
@@ -41,12 +54,44 @@ public class BuscarPacienteDialog extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
+		JComboBox cmbPaciente = new JComboBox();
+
+		cmbPaciente.setBounds(178, 107, 275, 43);
+		contentPanel.add(cmbPaciente);
+		
+		cmbPaciente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < consultas.size(); i++) {
+					try {
+						if (cmbPaciente.getSelectedItem().equals(con.selectWhere("paciente", "nombre", "idpaciente", consultas.get(i)[0]))) {
+							cmbPaciente.setSelectedItem(con.selectWhere("paciente", "nombre", "idpaciente", consultas.get(i)[0]));							
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+	
+			
+		
 		JLabel lblFondo = new JLabel("");
 		lblFondo.setIcon(new ImageIcon(BorrarUsuarioDialog.class.getResource("/fotos/buscar_paciente.PNG")));
 		lblFondo.setBounds(0, 0, 564, 421);
 		contentPanel.add(lblFondo);
+	
+			
+			
+		
 	}
 }
+	
+
+
+	
+
+
 				
 		
 	
